4ef3e97a42a333c1d210cd9207a61810022d9ded
My Doan <mydoanha278@gmail.com>
2025-12-01 19:00:25 +0100

    move

--- Assets/_Scripts/DesktopPlayerController.cs
+++ Assets/_Scripts/DesktopPlayerController.cs
@@ -7,10 +7,7 @@ public class DesktopArrowController : MonoBehaviour
     public float moveSpeed = 3f;
     public float gravity = -9.81f;
 
-    [Header("Turning (Arrow Keys)")]
-    public float turnSpeed = 120f;   // degrees per second
-
-    [Header("Look Up / Down (Mouse)")]
+    [Header("Mouse Look")]
     public Camera playerCamera;
     public float mouseSensitivity = 2f;
     public float verticalLookLimit = 80f;
@@ -36,51 +33,56 @@ public class DesktopArrowController : MonoBehaviour
 
     void Update()
     {
-        HandleTurning();
         HandleLook();
         HandleMovement();
     }
 
-    void HandleTurning()
-    {
-        float turn = 0f;
-
-        if (Input.GetKey(KeyCode.LeftArrow))
-            turn = -1f;
-        else if (Input.GetKey(KeyCode.RightArrow))
-            turn = 1f;
-
-        transform.Rotate(0f, turn * turnSpeed * Time.deltaTime, 0f);
-    }
-
     void HandleLook()
     {
+        // Mouse X = yaw (turn left/right)
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity;
+        // Mouse Y = pitch (look up/down)
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity;
 
+        // Rotate body around Y with mouse X
+        transform.Rotate(0f, mouseX, 0f);
+
+        // Pitch camera with mouse Y
         _pitch -= mouseY;
         _pitch = Mathf.Clamp(_pitch, -verticalLookLimit, verticalLookLimit);
-
         playerCamera.transform.localEulerAngles = new Vector3(_pitch, 0f, 0f);
     }
 
     void HandleMovement()
     {
-        float move = 0f;
+        float forward = 0f;
+        float strafe  = 0f;
 
+        // Forward / back with Up / Down arrows
         if (Input.GetKey(KeyCode.UpArrow))
-            move = 1f;
+            forward = 1f;
         else if (Input.GetKey(KeyCode.DownArrow))
-            move = -1f;
+            forward = -1f;
 
-        Vector3 direction = transform.forward * move * moveSpeed;
+        // Strafe left/right with Left / Right arrows
+        if (Input.GetKey(KeyCode.RightArrow))
+            strafe = 1f;
+        else if (Input.GetKey(KeyCode.LeftArrow))
+            strafe = -1f;
+
+        // Combine into world-space movement
+        Vector3 moveDir = (transform.forward * forward + transform.right * strafe);
+        if (moveDir.sqrMagnitude > 1f)
+            moveDir.Normalize();
+        moveDir *= moveSpeed;
 
         // Gravity
         if (_cc.isGrounded && _verticalVelocity < 0f)
             _verticalVelocity = -2f;
 
         _verticalVelocity += gravity * Time.deltaTime;
-        direction.y = _verticalVelocity;
+        moveDir.y = _verticalVelocity;
 
-        _cc.Move(direction * Time.deltaTime);
+        _cc.Move(moveDir * Time.deltaTime);
     }
 }
